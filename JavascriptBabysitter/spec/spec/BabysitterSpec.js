3a74725ae6ed49b91a37f10c463ad319fa93ac8c
William Land <keth45406@gmail.com>
2019-01-14 17:57:41 -0500

    Added normalization of arrival time.

--- JavascriptBabysitter/spec/spec/BabysitterSpec.js
+++ JavascriptBabysitter/spec/spec/BabysitterSpec.js
@@ -1,45 +1,73 @@
 describe ("A Babysitter", function(){
+	
 	describe("it accepts an arrival time" , function(){
-		it("of 1 and returns it.", function(){
-			var expected = Babysitter(1);
-			expect(expected.arrival).toEqual(1);
+		
+		it("of 5 and normalizes it.", function(){
+			var expected = Babysitter(5);
+			expect(expected.arrival).toEqual(17);
 		});
+		
+		it("of 1 and denys it.", function(){
+			var expected = Babysitter(1);
+			expect(expected.arrival).toEqual("You cannot arrive until 5 pm.");
+		});
+		
 		it("of 2 and returns it.", function(){
 			var expected = Babysitter(2);
-			expect(expected.arrival).toEqual(2);
+			expect(expected.arrival).toEqual("You cannot arrive until 5 pm.");
 		});
+		
+		it("of 2 and returns it.", function(){
+			var expected = Babysitter(2);
+			expect(expected.arrival).toEqual("You cannot arrive until 5 pm.");
+		});
+		
 	});
 	describe("it accepts a departure time",function(){
 
-		it("of 2 and returns it.",function(){
-			var expected = Babysitter(1,2);
-			expect(expected.departure).toEqual(2);
-		});
-		it("of 4 and returns it.",function(){
-			var expected = Babysitter(1,4);
-			expect(expected.departure).toEqual(4);
-		});
+//		it("of 2 and returns it.",function(){
+//			var expected = Babysitter(5,2);
+//			expect(expected.departure).toEqual(2);
+//		});
+//		
+//		it("of 4 and returns it.",function(){
+//			var expected = Babysitter(5,4);
+//			expect(expected.departure).toEqual(4);
+//		});
+//		
 	});
 	describe("it accepts a bedtime",function(){
 
-		it("of 3 and returns it.",function(){
-			var expected = Babysitter(1,2,3);
-			expect(expected.bedtime).toEqual(3);
-		});
-		it("of 6 and returns it.",function(){
-			var expected = Babysitter(1,4,6);
-			expect(expected.bedtime).toEqual(6);
-		});
-		it("will not allow bedtimes of midnight",function(){
-			var expected = Babysitter(1,4,12);
-			expect(expected.pay).toEqual("Please reenter a valid bedtime.");
-		});
+//		it("of 3 and returns it.",function(){
+//			var expected = Babysitter(5,2,3);
+//			expect(expected.bedtime).toEqual(3);
+//		});
+//		
+//		it("of 6 and returns it.",function(){
+//			var expected = Babysitter(5,4,6);
+//			expect(expected.bedtime).toEqual(6);
+//		});
+//		
+//		it("and ensures it is not midnight",function(){
+//			var expected = Babysitter(5,4,12);
+//			expect(expected.pay).toEqual("Please reenter a valid bedtime.");
+//		});
+//		
+//		it("and ensures it is not greater than 12",function(){
+//			var expected = Babysitter(5,4,13);
+//			expect(expected.pay).toEqual("Please reenter a valid bedtime.");
+//		});
+//		
+//		it("and ensures it is not earlier than 5pm",function(){
+//			var expected = Babysitter(5,4,13);
+//			expect(expected.pay).toEqual("Please reenter a valid bedtime.");
+//		});
 	});
 	describe("when given a babysitters arrival and departure times",function(){
 
-		it("it will return the difference",function(){
-			var expected = Babysitter(1,4,3);
-			expect(expected.pay).toEqual(3);
-		});
+//		it("it will return the difference",function(){
+//			var expected = Babysitter(5,4,3);
+//			expect(expected.pay).toEqual(3);
+//		});
 	});	
 });
--- JavascriptBabysitter/spec/src/Babysitter.js
+++ JavascriptBabysitter/spec/src/Babysitter.js
@@ -6,18 +6,31 @@ var Babysitter =( function(arrival, departure, bedtime){
 	var departure = departure;
 	var bedtime = bedtime;
 	
+	
+	self.arrival = normalizeArrivalTime(arrival);
+	self.departure = departure;
+	self.bedtime = bedtime;
+	
 		function calculate(){
-			if(bedtime==12){
+			if(bedtime>=12){
 				return "Please reenter a valid bedtime."
-			}else {return departure-arrival;}
+			}else {return arrival-departure;}
+		};
+		
+		function normalizeArrivalTime(arrival){
+			arrival += 12;
+			if (arrival<=16){
+				return "You cannot arrive until 5 pm.";
+			}else
+				return arrival;
 		};
 		
 	
+	
+		
 	self.pay = calculate();
 	
-	self.arrival = arrival;
-	self.departure = departure;
-	self.bedtime = bedtime;
+	
 	
 	return self;
 });
